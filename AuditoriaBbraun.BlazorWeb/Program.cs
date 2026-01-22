cf515cfb62c280269ac35f0ddb9b7e505596d74a
Cristian Anjari <93675228+SnakeJuice@users.noreply.github.com>
2026-01-22 11:41:57 -0300

    Agregado interfaz Web
    + Agregado estilos e imagenes para la página
    + Agregado hub dummy
    + Agregado login dummy

--- AuditoriaBbraun.BlazorWeb/Program.cs
+++ AuditoriaBbraun.BlazorWeb/Program.cs
@@ -6,13 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// HttpClient hacia la API
+// Ajustar ApiBaseUrl en appsettings.json para cambiar el endpoint o puerto de la API!!!!!
+builder.Services.AddHttpClient("api", client =>
+{
+    var baseUrl = builder.Configuration["ApiBaseUrl"] ?? "/";
+    client.BaseAddress = new Uri(baseUrl, UriKind.RelativeOrAbsolute);
+});
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("api"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
